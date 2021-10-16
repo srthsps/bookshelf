e214a69206315fb39458fd1d0dbd117047921cb3
sarathsps <srthsps22@gmail.com>
2021-10-16 19:25:36 +0530

    codedeploy pipeline

---
+++ after.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# cd /home/ubuntu/realbuy
+npm run build
---
+++ appstart.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/ubuntu/realbuy
+mkdir /var/www/realbuy
+cp -r build/* /var/www/realbuy
+systemctl restart nginx
