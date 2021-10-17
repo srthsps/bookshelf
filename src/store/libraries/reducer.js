f5ed63fbb85f0f8019b600242998da64d8a714aa
sarathsps <srthsps22@gmail.com>
2021-10-18 03:45:08 +0530

    added members data

--- src/store/libraries/reducer.js
+++ src/store/libraries/reducer.js
@@ -1,11 +1,13 @@
 import { libraryData } from './states/libraryData'
 import { bookData } from './states/bookData'
 import { employeeData } from './states/employeeData'
+import { membersData } from './states/membersData'
 
 const INIT_STATE = {
     libraryData,
     bookData,
-    employeeData
+    employeeData,
+    membersData
 }
 
 const libraryReducer = (state= INIT_STATE) =>{
